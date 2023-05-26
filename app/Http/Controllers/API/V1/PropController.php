09a6f80750eb5564985240091c0be2dbdf8c7b46
nowalor <no@walor.io>
2023-05-26 22:11:45 +0200

    feat: progress with PropController.store

--- app/Http/Controllers/API/V1/PropController.php
+++ app/Http/Controllers/API/V1/PropController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\API\V1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\API\V1\StorePropRequest;
+use App\Models\Label;
 use App\Models\Prop;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class PropController extends Controller
@@ -16,9 +18,38 @@ class PropController extends Controller
         return response()->json($props);
     }
 
-    public function store(StorePropRequest $request)
+    public function store(StorePropRequest $request)// : JsonResponse
     {
-        //
+        $validated = $request->validated();
+
+        return $validated;
+
+        if(empty($validated)) {
+            return response()->json(['message' => 'No data provided'], 422);
+        }
+
+        $labelIds = [];
+
+        if ($request->has('labels')) {
+            return 'has labels';
+            $labels = $validated['labels'];
+
+            foreach ($labels as $label) {
+                return $label;
+                $label = Label::firstOrCreate(['name' => $label]);
+                $labelIds[] = $label->id;
+            }
+
+            unset($validated['labels']);
+        }
+
+        $prop = Prop::create($validated);
+
+        if (!empty($labelIds)) {
+            $prop->labels()->syncWithoutDetaching($labelIds);
+        }
+
+        return response()->json($prop, 201);
     }
 
     /**
--- app/Http/Requests/API/V1/StorePropRequest.php
+++ app/Http/Requests/API/V1/StorePropRequest.php
@@ -8,7 +8,7 @@ class StorePropRequest extends FormRequest
 {
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     public function rules(): array
